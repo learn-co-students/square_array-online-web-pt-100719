4c14e2f9592c8d1ba26b5831033d3e1462316a38
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-15 06:09:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square!
+def square
   numbers = [1,2,3,4]
     square_array(numbers)
        self.numbers! {|num| num ** 2}
 end
   
-def square!
+def square
     new numbers = [5,6,7,8]
       arr = []
   array.each { |new_numbers| arr << new_numbers ** 2 }
