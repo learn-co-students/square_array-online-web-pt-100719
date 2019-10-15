c0bed65ce044ef74d00b084b2d023f073b5a8220
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-15 06:22:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,12 @@
-def square_array
-  numbers = [1,2,3,4]
-    arr = []
-  square_array.each {arr << numbers ** 2 }
-  arr
+def square_array(array)
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
-  
-def square_array
-    new numbers = [5,6,7,8]
-      arr = []
-  square_array.each {arr << new_numbers ** 2 }
-  arr
-end
+
+
 
