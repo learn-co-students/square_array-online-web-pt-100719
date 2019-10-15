c18f8e6f5df06193cc1916250bb2df1a23947e9a
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-15 06:17:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,14 @@
 def square_array
   numbers = [1,2,3,4]
     arr = []
-  square_array.each { |numbers| arr << numbers ** 2 }
+  square_array.each {arr << numbers ** 2 }
   arr
 end
   
 def square_array
     new numbers = [5,6,7,8]
       arr = []
-  square_array.each { |new_numbers| arr << new_numbers ** 2 }
+  square_array.each {arr << new_numbers ** 2 }
   arr
 end
 
