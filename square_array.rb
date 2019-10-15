cb9ed04219dbe9733af8a46578fb673946917264
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-15 06:04:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,9 @@
-def 
+def square!
   numbers = [1,2,3,4]
   
   square_array(numbers)
-  
-  def square!
-     arr = []    
+
        self.numbers! {|num| num ** 2}
-    arr
   end
   
   new numbers = [5,6,7,8]
